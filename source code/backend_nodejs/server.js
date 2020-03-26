09a609d7e3757f2d933f37bdc861630d798f1ee3
LiWaiYip <1155115790@link.cuhk.edu.hk>
2020-03-26 16:50:14 +0800

    added nodejs code

---
+++ source code/backend_nodejs/server.js
@@ -0,0 +1,122 @@
+const express = require('express'); const app = express();
+const bodyParser = require("body-parser");
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var serverURL = 'mongodb://csci3100_15:csci3100_15@localhost/csci3100';
+mongoose.connect(serverURL);
+
+/* Connect to the database: begins */
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function() {
+  console.log('Connection is open...');
+});
+/* Connect to the database: ends */
+
+/* Setup database tables: begins */
+
+var UserSchema = mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+var User = mongoose.model('User', UserSchema);
+
+var RestaurantSchema = mongoose.Schema({
+  restId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  tag: {
+    type: [String]
+  },
+  likes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  dislikes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  comments: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    required: true,
+    default: []
+  }
+});
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+
+var CommentSchema = mongoose.Schema({
+  commentId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  postBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  postOn: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Restaurant',
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  dislikes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+var Comment = mongoose.model('Comment', CommentSchema);
+
+/* Setup database tables: ends */
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.all('/*', function (req, res) {
+    res.send("Hello World from CSCI 3100 Group 15 Backend Server!");
+});
+
+// listen to port 3000
+const server = app.listen(3000);
