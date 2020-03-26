06aca16c87a28db23d6ba87c20f5b8e5858c0dc7
LiWaiYip <1155115790@link.cuhk.edu.hk>
2020-03-26 13:34:59 +0800

    added reg form

--- source code/js/script.js
+++ source code/js/script.js
@@ -1,6 +1,7 @@
 // When the page load
 function init() {
   $("#loginForm").hide();
+  $("#registerForm").hide();
 }
 
 // When user clicks the login button in the page
@@ -14,7 +15,41 @@ function closeLoginForm() {
   $("#loginPassword").val('');
 }
 
-// When user clicks outside the login form
+// Close the register form
+function closeRegisterForm() {
+  $("#registerForm").fadeOut();
+}
+
+// When user clicks the login button in the login form.
+function loginSubmit(e) {
+  e.submit();
+  closeLoginForm();
+}
+
+
+
+
+// When user clicks the register button in the page
+function register() {
+  $("#registerForm").fadeIn();
+}
+
+
+// When user clicks the register button in the login form.
+function registerSubmit(e) {
+  if ($("#registerPassword").val() != $("#registerRePassword").val()) {
+    alert('Two Password Must Be Equal');
+    return false;
+  }
+  else {
+    closeRegisterForm();
+    return true;
+  }
+
+}
+
+
+// When user clicks outside the login/register form
 $(document).mouseup(function(e)
 {
     var container = $("#loginForm");
@@ -24,10 +59,12 @@ $(document).mouseup(function(e)
     {
         closeLoginForm();
     }
-});
 
-// When user clicks the login button in the login form.
-function loginSubmit(e) {
-  e.submit();
-  closeLoginForm();
-}
+    var container = $("#registerForm");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0)
+    {
+        closeRegisterForm();
+    }
+});
